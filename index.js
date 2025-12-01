d45ee6f26d61f1f1985dfffb26c77ee8cea2cc17
Eduardo <eduardoamorima@gmail.com>
2025-11-30 22:15:03 -0300

    Meu terceiro commit

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 console.log ("desafio do felipao")
 console.log ("primeira atualização de programa no github")
+let nome = "Eduardo"
+console.log (nome)
+
--- src/index.js
+++
@@ -1,4 +0,0 @@
-console.log ("desafio do felipao")
-console.log ("primeira atualização de programa no github")
-let nome = "Eduardo"
-console.log (nome)
